5605c95c442cf903b2b9aeec7e7605cf1e602b60
mamba <928477320@qq.com>
2019-09-05 21:15:15 +0800

    个人中心

---
+++ personCenter/personCenter.js
@@ -0,0 +1,15 @@
+if (window.CSS) {
+    CSS.registerProperty({
+        name: '--x',
+        syntax: '<length-percentage>',
+        initialValue: '0%',
+        inherits: true
+    });
+
+    CSS.registerProperty({
+        name: '--y',
+        syntax: '<length-percentage>',
+        initialValue: '0%',
+        inherits: true
+    });
+}
